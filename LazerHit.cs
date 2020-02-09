bfe47383d62bca3668903f53f697b8734864c623
Sydney Keating <bookmasterdc@gmail.com>
2020-02-08 22:54:20 -0500

    Laser Hit

--- LazerHit.cs
+++ LazerHit.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class LazerHit : MonoBehaviour
 {
     private GameObject lazer;
+
+   private float timer2;
+    public GameObject player;
+
+    private bool hit;
+    
     // Start is called before the first frame update
     void Start()
     {
         lazer = GetComponent<GameObject>();
+        player = GameObject.Find("Robber");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer2 += Time.deltaTime;
+        if(hit){
+          if(timer2 >= 0.3f){
+             player.GetComponent<SpriteRenderer>().color = UnityEngine.Color.white;
+             }
+              else{
+                player.GetComponent<SpriteRenderer>().color = UnityEngine.Color.red;
+              }
+        }
     }
     void OnCollisionEnter2D(Collision2D col)
     {   
         if(this.gameObject.name.Equals("Toplazer(Clone)") && col.collider.tag.Equals("Player")){      
         PlayerHealth.health -= 5;
          PlayerHealth.displayhealth -= 5;
-         Debug.Log(PlayerHealth.health);
+         displayhit();
     }
     else if(this.gameObject.name.Equals("Coin") ||  this.gameObject.name.Equals("Coin(Clone)")){      
         if (col.collider.tag.Equals("Player")){
@@ -33,6 +48,11 @@ public class LazerHit : MonoBehaviour
         Destroy(lazer);
        }
 
+    }
+
+    public void displayhit(){
+       hit = true;
+       timer2 = 0;
     }
     
    
