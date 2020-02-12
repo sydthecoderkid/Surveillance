25d6e870ceaeb7ab01798b9f7373bebf73b200bf
Sydney Keating <bookmasterdc@gmail.com>
2020-02-11 20:58:07 -0500

    Levels, Lasers, Coins

---
+++ PlaySounds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour
+{
+        public AudioSource lazersound;
+      public AudioSource coinsound;
+   
+         
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(LazerHit.playlazersound){
+          lazersound.Play();
+            LazerHit.playlazersound = false;
+       }
+        if(LazerHit.playcoinsound){
+           coinsound.Play();
+            LazerHit.playcoinsound = false;
+       }
+    }
+
+   
+  
+}
